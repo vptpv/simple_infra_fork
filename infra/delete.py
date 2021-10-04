7a971e06025907f229442f96eee2f5040f549175
savelbl4 <savelbl4@gmail.com>
2021-10-04 11:53:18 +0300

    микро-правка удаления

--- infra/delete.py
+++ infra/delete.py
@@ -20,7 +20,8 @@ def hosts_by_sap(auth, reader):
             r = requests.delete(url, cookies = auth.cookies, data=json.dumps(payload), headers=auth.headers)
             messages.append(f"{hostName} {line['asset_tag']} - удалён")
             print('.', end='')
-    pprint('\n'+messages)
+    print('\n')
+    pprint(messages)
 
 def hosts_by_name(auth, reader):
     messages = []
