f95f9ae092aa77f973eb9d5f141dd3bf4a2156e3
savelbl4 <savelbl4@gmail.com>
2021-12-07 17:31:47 +0300

    шоб пайчарм сильно не ругался 0.0.4

--- infra/delete.py
+++ infra/delete.py
@@ -1,45 +1,45 @@
-import requests, json
+import requests
+import json
 from pprint import pprint
-from infra import make
+
 
 def hosts_by_sap(auth, reader):
     messages = []
     for line in reader:
         url = f"{auth.api_domain}/api/hosts?$filter=SapInventory eq '{line['asset_tag']}'"
-        r = requests.get(url, cookies = auth.cookies)
-        json_1 = json.loads(r.text)
-        try:
-            hostId = str(json_1[0]["Id"])
-            hostName = str(json_1[0]["HostName"])
-        except IndexError:
+        r = requests.get(url, cookies=auth.cookies)
+        json_1 = json.loads(r.text)[0] if len(json.loads(r.text)) > 0 else {}
+        hostid = json_1.get('Id', '')
+        hostname = json_1.get('HostName', '')
+        if hostid == '':
             messages.append(f"\n{line['asset_tag']} - не в стойке")
             print('`', end='')
         else:
-            url = f"{auth.api_domain}/api/hosts/{hostId}"
+            url = f"{auth.api_domain}/api/hosts/{hostid}"
             payload = {"Task": line['task']}
-            r = requests.delete(url, cookies = auth.cookies, data=json.dumps(payload), headers=auth.headers)
-            messages.append(f"{hostName} {line['asset_tag']} - удалён")
+            r = requests.delete(url, cookies=auth.cookies, data=json.dumps(payload), headers=auth.headers)
+            messages.append("{} {} - удалён({})".format(hostname, line['asset_tag'], r.status_code))
             print('.', end='')
     print('\n')
     pprint(messages)
 
+
 def hosts_by_name(auth, reader):
     messages = []
     for line in reader:
-        url = f"{auth.api_domain}/api/hosts?$filter=HostName eq '{line['new_name']}'"
-        r = requests.get(url, cookies = auth.cookies)
-        json_1 = json.loads(r.text)
-        try:
-            hostId = str(json_1[0]["Id"])
-            hostName = str(json_1[0]["HostName"])
-        except IndexError:
-            messages.append(f"{line['new_name']} - не в стойке")
+        hostname = line.get('new_name', '')
+        url = f"{auth.api_domain}/api/hosts?$filter=HostName eq '{hostname}'"
+        r = requests.get(url, cookies=auth.cookies)
+        json_1 = json.loads(r.text)[0] if len(json.loads(r.text)) > 0 else {}
+        hostid = json_1.get('Id', '')
+        if hostid == '':
+            messages.append(f"{hostname} - не в стойке")
             print('`', end='')
         else:
-            url = f"{auth.api_domain}/api/hosts/{hostId}"
+            url = f"{auth.api_domain}/api/hosts/{hostid}"
             payload = {"Task": line['task']}
-            r = requests.delete(url, cookies = auth.cookies, data=json.dumps(payload), headers=auth.headers)
-            messages.append(f"{hostName} - удалён")
+            r = requests.delete(url, cookies=auth.cookies, data=json.dumps(payload), headers=auth.headers)
+            messages.append('{} - удалён({})'.format(hostname, r.status_code))
             print('.', end='')
     print('\n')
     pprint(messages)
